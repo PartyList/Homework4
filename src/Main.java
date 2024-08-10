0e4f1611775d40eff75c87efcc11c281bd4fd911
Taiyo <taiyo.yemin@campus.technion.ac.il>
2024-08-10 12:20:36 +0300

    updated homework files

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,81 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Iterator;
+
+class MyCloneable implements Cloneable{
+    private int num;
+
+    public MyCloneable(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "MyCloneable: " + num;
+    }
+
+    @Override
+    public int hashCode() {
+        return num;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof MyCloneable)){
+            return false;
+        }
+        MyCloneable other = (MyCloneable) obj;
+        return num == other.num;
+    }
+
+    @Override
+    public MyCloneable clone() {
+        try {
+            return (MyCloneable) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            System.out.println("Test 1 starts");
+            test1();
+        }catch (Exception e){
+            System.out.println("exception " + e);
+        }finally {
+            System.out.println("Test 1 done");
+            System.out.println("--------------------------------------------");
+        }
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void test1(){
+        IsraeliQueue<MyCloneable> queue = new IsraeliQueue<>();
+        queue.add(new MyCloneable(1));
+        queue.add(new MyCloneable(2));
+        queue.add(new MyCloneable(3));
+
+        IsraeliQueue<MyCloneable> clonedQueue = queue.clone();
+
+        Iterator<MyCloneable> iterator = queue.iterator();
+        Iterator<MyCloneable> clonedIterator = clonedQueue.iterator();
+
+        while (iterator.hasNext() && clonedIterator.hasNext()){
+            MyCloneable fromOrg = iterator.next();
+            MyCloneable fromCloned = clonedIterator.next();
+
+            System.out.println(fromOrg);
+            System.out.println(fromCloned);
+            System.out.println(fromOrg.equals(fromCloned));
+            System.out.println(fromOrg == fromCloned);
         }
     }
 }
